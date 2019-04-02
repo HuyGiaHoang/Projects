84bf2c7a36e9a1eddf73f6648b2fcf226f668a9a
Huy Gia Hoang <giahuy281000@gmail.com>
2019-04-02 09:28:01 +0100

    modified main.c for recording

--- main.c
+++ main.c
@@ -2,18 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 #include "screen.h"
+#include "sound.h"
+
 
 int main(){
-	int dec[COL], i;				//80 pieces of sound decibels
+	FILE *f;
+	short sd[80000];
+	for(;;){
+	system(RCMD);
+	f = fopen("test.wav", "r");
+		if(f==NULL){
+			printf("Cannot open the file\n");
+			return 1;
+		}
+//	int dec[COL], i;					//80 pieces of sound decibels
 
-	srand(time(NULL));
-	for(i=0; i<COL; i++) dec[i]= rand()%70+30;
+//	srand(time(NULL));
+//	for(i=0; i<COL; i++) dec[i]= rand()%70+30;
 
-	clearScreen();
+		clearScreen();
+		setColors(RED, bg(PINK));
+//	barChart(dec);
 
-	setColors(RED, bg(YELLOW));
-	barChart(dec);
+		struct WAVHDR hdr;
+		fread(&hdr, sizeof(hdr), 1, f);		// read WAV header
+		fread(&sd, sizeof(sd), 1, f);		// read WAV data
+		fclose(f);
 
+		displayWAVHDR(hdr);
+		displayWAVDATA();
+	}
 	resetColors();
 	getchar();
 }
